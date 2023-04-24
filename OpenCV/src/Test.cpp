01e55a890e32247509af16ab0b40ebf2aa1d7e29
weifuchunyang <1306701599@qq.com>
2023-04-24 20:48:44 +0800

    添加项目文件。

---
+++ OpenCV/src/Test.cpp
@@ -0,0 +1,16 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	string path = "Resources/test.png";
+	Mat img = imread(path);
+	imshow("image", img);
+	waitKey(0);
+	return 0;
+}
