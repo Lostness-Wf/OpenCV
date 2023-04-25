e4d72e8bc85a3b4833ccd4c0307b7cc258ff5fd0
weifuchunyang <1306701599@qq.com>
2023-04-25 13:53:10 +0800

    detect dhapes

--- OpenCV/src/Chapter2.cpp
+++ OpenCV/src/Chapter2.cpp
@@ -8,33 +8,41 @@ using namespace cv;
 //////////   Basic Functions   //////////
 int main()
 {
-	std::string path = "Resources/test.png";
-	Mat img = imread(path);
+	VideoCapture cap(0);
+	Mat img;
+
+	//std::string path = "Resources/test.png";
+	//Mat img = imread(path);
 	Mat imgGary;
 	Mat imgBlur;
 	Mat imgCanny;
 	Mat imgDil;
 	Mat imgErode;
 
-	//转换为灰度
-	cvtColor(img, imgGary, COLOR_BGR2GRAY);
-	//高斯模糊
-	GaussianBlur(img, imgBlur, Size(7, 7), 5, 0);
-	//轮廓检测
-	Canny(imgBlur, imgCanny, 20, 45);
-
-	//放大轮廓线
-	Mat kernal = getStructuringElement(MORPH_RECT, Size(5, 5));
-	dilate(imgCanny, imgDil, kernal);
-	//侵蚀轮廓线
-	dilate(imgDil, imgErode, kernal);
-
-	imshow("image", img);
-	imshow("image Gary", imgGary);
-	imshow("image Blur", imgBlur);
-	imshow("image Canny", imgCanny);
-	imshow("image Dilation", imgDil);
-	imshow("image Erode", imgErode);
-	waitKey(0);
+	while (true)
+{
+	cap.read(img);
+	
+		//转换为灰度
+		cvtColor(img, imgGary, COLOR_BGR2GRAY);
+		//高斯模糊
+		GaussianBlur(imgGary, imgBlur, Size(7, 7), 5, 0);
+		//轮廓检测
+		Canny(imgBlur, imgCanny, 25, 45);
+	
+		//放大轮廓线
+		//Mat kernal = getStructuringElement(MORPH_RECT, Size(5, 5));
+		//dilate(imgCanny, imgDil, kernal);
+		//侵蚀轮廓线
+		//dilate(imgDil, imgErode, kernal);
+	
+		imshow("image", img);
+		imshow("image Gary", imgGary);
+		imshow("image Blur", imgBlur);
+		imshow("image Canny", imgCanny);
+		//imshow("image Dilation", imgDil);
+		//imshow("image Erode", imgErode);
+		waitKey(1);
+}
 	return 0;
 }
