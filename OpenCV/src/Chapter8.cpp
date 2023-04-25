7a27e49e8911071b56b60be01dddd79690b69313
weifuchunyang <1306701599@qq.com>
2023-04-25 14:56:15 +0800

    face detect

---
+++ OpenCV/src/Chapter8.cpp
@@ -0,0 +1,36 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/objdetect.hpp>
+
+#include <iostream>
+
+using namespace cv;
+
+//////////   Images   //////////
+int main()
+{
+	VideoCapture cap(0);
+	Mat img;
+
+	CascadeClassifier faceCascade;
+	faceCascade.load("Resources/haarcascade_frontalface_default.xml");
+	if (faceCascade.empty()) { std::cout << "XML file not loaded" << std::endl; }
+
+	std::vector<Rect> faces;
+
+	while (true)
+	{
+		cap.read(img);
+
+		faceCascade.detectMultiScale(img, faces, 1.3, 10);
+		for (int i = 0; i < faces.size(); ++i)
+		{
+			rectangle(img, faces[i].tl(),faces[i].br(), Scalar(255, 0, 255), 3);
+		}
+
+		imshow("image", img);
+		waitKey(1);
+	}
+	return 0;
+}
