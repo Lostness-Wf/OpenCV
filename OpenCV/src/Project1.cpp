9a9f89f2ae391369906098a6efe332ef8cf9638d
weifuchunyang <1306701599@qq.com>
2023-04-26 14:30:41 +0800

    detect color and draw on canvas

--- OpenCV/src/Project1.cpp
+++ OpenCV/src/Project1.cpp
@@ -6,13 +6,13 @@
 using namespace std;
 using namespace cv;
 
-vector<vector<int>> myColors{ {132, 149, 45, 191, 767, 224} /*purple*/ };
-vector<Scalar> myColorValus{ {255, 0, 255} };
-
 Mat img, imgHSV, mask;
 
+vector<vector<int>> myColors{ {132, 149, 45, 191, 767, 224} /*purple*/ };
+vector<Scalar> myColorValues{ {255, 0, 255} };
+vector<vector<int>> newPoints;
 
-void GetContours(Mat imgDil)
+Point GetContours(Mat imgDil)
 {
 
 	vector<vector<Point>> contours;
@@ -20,30 +20,38 @@ void GetContours(Mat imgDil)
 
 	findContours(imgDil, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
 
+	vector<vector<Point>> conPoly(contours.size());
+	vector<Rect> boundRect(contours.size());
+
+	Point myPoint(0.0);
 
 	for (int i = 0; i < contours.size(); i++)
 	{
-		vector<vector<Point>> conPoly(contours.size());
-		vector<Rect> boundRect(contours.size());
-
 		int area = contourArea(contours[i]);
 		cout << area << endl;
 
-		if (area > 10)
+		if (area > 5)
 		{
 			float peri = arcLength(contours[i], true);
 			//近似多边形
-			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, false);
+			approxPolyDP(contours[i], conPoly[i], 0.02 * peri, true);
+			//绘制轮廓
 			drawContours(img, conPoly, i, Scalar(255, 0, 255), 2);
 
 			boundRect[i] = boundingRect(conPoly[i]);
+			myPoint.x = boundRect[i].x + boundRect[i].width / 2;
+			myPoint.y = boundRect[i].y;
+
+			//绘制包裹框
 			rectangle(img, boundRect[i].tl(), boundRect[i].br(), Scalar(0, 255, 0), 3);
 		}
 	}
+	return myPoint;
 }
 
-void FindColor(Mat img)
+vector<vector<int>> FindColor(Mat img)
 {
+	//转换为灰度
 	cvtColor(img, imgHSV, COLOR_RGB2HSV);
 
 	for (int i = 0; i < myColors.size(); ++i)
@@ -53,7 +61,21 @@ void FindColor(Mat img)
 		inRange(imgHSV, lower, upper, mask);
 		imshow(to_string(i), mask);
 
-		GetContours(mask);
+		Point myPoint = GetContours(mask);
+
+		if (myPoint.x != 0 && myPoint.y != 0)
+		{
+			newPoints.push_back({ myPoint.x, myPoint.y, i });
+		}
+	}
+	return newPoints;
+}
+
+void DrawOnCanvas(vector<vector<int>> newPoints, vector<Scalar> myColorValues)
+{
+	for (int i = 0; i < newPoints.size(); ++i)
+	{
+		circle(img, Point(newPoints[i][0], newPoints[i][1]), 3, myColorValues[newPoints[i][2]], FILLED);
 	}
 }
 
@@ -65,7 +87,8 @@ int main()
 	while (true)
 	{
 		cap.read(img);
-		FindColor(img);
+		newPoints = FindColor(img);
+		DrawOnCanvas(newPoints, myColorValues);
 
 		imshow("Image", img);
 		waitKey(1);
